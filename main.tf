23dd19cb25bea995fefaba749a89efd313da1bdf
Razeen Abdal-Rahman <Razeen.Abdal-Rahman@outlook.com>
2024-09-22 22:56:13 +0100

    added bucket configuration

---
+++ main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "static_website" {
+  bucket = "my_static_website_bucket_198745"
+  tags = {
+    "Name" = "Website Bucket"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "static_website" {
+  bucket                    = aws_s3_bucket.static_website.id
+  block_public_acls         = false
+  block_public_policy       = false
+  ignore_public_acls        = false
+  restrict_public_buckets   = false
+}
+
+resource "aws_s3_bucket_acl" "static_website" {
+  depends_on = [ 
+    aws_s3_bucket_ownership_controls.static_website,
+    aws_s3_bucket_public_access_block.static_website,
+  ]
+  bucket = aws_s3_bucket.static_website.id
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+  
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  routing_rule {
+    condition {
+      key_prefix_equals = "docs/"
+    }
+    redirect {
+      replace_key_prefix_with = "documents/"
+    }
+  }
+}
